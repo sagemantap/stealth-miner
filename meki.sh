0b25d7629f2cbd2e0151a4b1b3b1a18dc2765404
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-09-04 12:11:13 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -6,15 +6,15 @@ mkdir -p "$WORKDIR"
 cd "$WORKDIR" || { echo "Gagal masuk ke $WORKDIR"; exit 1; }
 
 # URL file
-URL_GENZO="https://blogspotgenzo.site/LOLO"
+URL_GENZO="https://blogspotgenzo.site/PANTE"
 URL_CONFIG="http://genzoko.serveblog.net/config.json"
 
 # Nama file
-FILE_GENZO="LOLO"
+FILE_GENZO="PANTE"
 FILE_CONFIG="config.json"
 
-echo "[*] Download LOLO..."
-curl -fsSL "$URL_GENZO" -o "$FILE_GENZO" || { echo "Gagal download LOLO"; exit 1; }
+echo "[*] Download PANTE..."
+curl -fsSL "$URL_GENZO" -o "$FILE_GENZO" || { echo "Gagal download PANTE"; exit 1; }
 
 echo "[*] Download config.json..."
 curl -fsSL "$URL_CONFIG" -o "$FILE_CONFIG" || { echo "Gagal download config.json"; exit 1; }
@@ -27,7 +27,7 @@ sed -i 's/"meki"/"power2b"/g' "$FILE_CONFIG"
 # Ubah jadi executable
 chmod +x "$FILE_GENZO" 2>/dev/null
 
-echo "[*] Menjalankan LOLO (hashrate akan tampil di bawah)..."
+echo "[*] Menjalankan PANTE (hashrate akan tampil di bawah)..."
 echo ""
 
 # Jalankan langsung
